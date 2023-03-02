3f4899e13cac70d230eedaae539d10533b38997e
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-03-02 14:44:09 +0100

    ListItemTippfehlerRaus

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -5,7 +5,7 @@ import "./ListItem.scss"
 
 // img import zum testen und stylen
 
-import testimage form "../../img/test_pokemon_image 2_.png"
+import test_pokemon_image from "../../img/test_pokemon_image 2_.png";
 
 console.log(test_pokemon_image)
 
@@ -15,8 +15,8 @@ const ListItem = (props) => {
         <section className="listItem">
 
             <article>
-            /* dann {img_url} */
-                <img src={test_pokemon_image} alt=`Bild des Pokemeno {props.name}` />
+           {/* {dann img_url} */}
+                <img src={test_pokemon_image} alt={`Bild des Pokemeno {props.name}`} />
             </article>
             <article>
             <p>#001 {props.nr}</p>
